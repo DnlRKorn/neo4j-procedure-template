face79be987ab985ef27ee56a99d4d1f2c072649
dnlrkorn <dnlrkorn@gmail.com>
2021-04-29 16:21:37 -0400

    Produced outline of method for promiscuity calculations. Created return object for Promscuity to neo4j. Created and ran test of promscuity outline method without causing to crash.

--- src/main/java/example/Promiscuity.java
+++ src/main/java/example/Promiscuity.java
@@ -10,11 +10,8 @@ import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Stream;
-import java.util.PriorityQueue;
 
 //import Comparator.comparingInt;
 
@@ -29,73 +26,74 @@ public class Promiscuity {
     @Context
     public Log log;
 
-
+//* @param k length of paths. If k=2 s->v1->v2->t would be a valid path.
+//* @param k length of paths. If k=2 s->v1->v2->t would be a valid path.
     /**
      * This procedure takes a Node and gets the relationships going in and out of it
-     *
-     * @param node  The node to get the relationships for
+     * @param sourceNode  node to start promiscuity search from
+     * @param tailNode node to end promiscuity search at
+
      * @return  A RelationshipTypes instance with the relations (incoming and outgoing) for a given node.
      */
     @Procedure(value = "example.promiscuityPath")
     @Description("Get the different relationships going in and out of a node.")
-    public Stream<RelationshipTypes2> promiscuityPath(@Name("node") Node node) {
-        List<String> outgoing = new ArrayList<>();
-        List<String> incoming = new ArrayList<>();
+    public Stream<Output> promiscuityPath(
+            @Name("sourceNode") Node sourceNode,
+            @Name("tailNode") Node tailNode) {
+        int k = 2;
+        PriorityQueue<Entry> priorityQueue = new PriorityQueue<>();
 
-       node.getRelationships(Direction.OUTGOING).iterator()
-                .forEachRemaining(rel -> printDegree(rel.getOtherNode(node)));
+       sourceNode.getRelationships().iterator()
+                .forEachRemaining(rel -> AddToQueue(priorityQueue,rel.getOtherNode(sourceNode),0,1));
 
-//        node.getRelationships(Direction.INCOMING).iterator()
-//                .forEachRemaining(rel -> AddDistinct(incoming, rel));
+       int best_score = Integer.MAX_VALUE;
+       while(!priorityQueue.isEmpty()){
+           Entry head = priorityQueue.poll();
+           Node node = head.node;
+           if(head.degree >= best_score){
+               return Stream.of(new Output(best_score));
+           }
+           if(head.depth<=1) {
+               node.getRelationships().iterator()
+                       .forEachRemaining(rel -> AddToQueue(priorityQueue, rel.getOtherNode(node), 0, head.depth + 1));
+           }
 
-        return Stream.of(new RelationshipTypes2(incoming, outgoing));
+       }
+
+        return new ArrayList<Output>().stream();
     }
 
     /**
      * Prints degree of given node to console
-     *
      * @param node the node which we should print degree of
      */
     private void printDegree(Node node){
         System.out.println(node.getDegree());
     }
 
-
     /**
-     * This is the output record for our search procedure. All procedures
-     * that return results return them as a Stream of Records, where the
-     * records are defined like this one - customized to fit what the procedure
-     * is returning.
-     * <p>
-     * These classes can only have public non-final fields, and the fields must
-     * be one of the following types:
-     *
-     * <ul>
-     *     <li>{@link String}</li>
-     *     <li>{@link Long} or {@code long}</li>
-     *     <li>{@link Double} or {@code double}</li>
-     *     <li>{@link Number}</li>
-     *     <li>{@link Boolean} or {@code boolean}</li>
-     *     <li>{@link Node}</li>
-     *     <li>{@link Relationship}</li>
-     *     <li>{@link Path}</li>
-     *     <li>{@link Map} with key {@link String} and value {@link Object}</li>
-     *     <li>{@link List} of elements of any valid field type, including {@link List}</li>
-     *     <li>{@link Object}, meaning any of the valid field types</li>
-     * </ul>
+     * Creates entry for given node and appends to priority queue.
+     * @param priorityQueue  the queue which we are adding the Entry to.
+     * @param node the node which we should create Entry for and append to queue.
      */
-    public static class RelationshipTypes2 {
-        // These records contain two lists of distinct relationship types going in and out of a Node.
-        public List<String> outgoing;
-        public List<String> incoming;
+    private void AddToQueue(PriorityQueue<Entry> priorityQueue, Node node, int path_score, int depth ){
+        Entry e = new Entry(node.getDegree(), path_score, depth,node);
+        priorityQueue.add(e);
+    }
 
-        public RelationshipTypes2(List<String> incoming, List<String> outgoing) {
-            this.outgoing = outgoing;
-            this.incoming = incoming;
+    public static class Output {
+        public Number promiscuity_score;
+
+        public Output(Number promiscuity_score) {
+            this.promiscuity_score = promiscuity_score;
+        }
+
+        public Output(int promiscuity_score) {
+            this.promiscuity_score = (Number) promiscuity_score;
         }
     }
 
-    public static class Entry implements Comparable<Entry>{
+    public static class Entry implements Comparator<Entry>, Comparable<Entry>{
         // These entries contain information we need about nodes stashed on our priority queue.
         public int degree;
         public int path_score;
@@ -115,5 +113,11 @@ public class Promiscuity {
             return this.degree - o.degree;
         }
 
+        @java.lang.Override
+        public int compare(Entry e1, Entry e2) {
+            return e1.degree - e2.degree;
+        }
+
     }
+
 }
--- src/test/java/example/PromiscuityTests.java
+++ src/test/java/example/PromiscuityTests.java
@@ -1,12 +1,19 @@
 package example;
 
 import org.junit.jupiter.api.*;
-import org.neo4j.driver.*;
+import org.neo4j.driver.Record;
+import org.neo4j.driver.Config;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Session;
 import org.neo4j.harness.Neo4j;
 import org.neo4j.harness.Neo4jBuilders;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertEquals;
+import org.junit.Assert;
+
+
+import java.util.List;
+import java.util.PriorityQueue;
 
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -44,30 +51,76 @@ public class PromiscuityTests {
      */
     @Test
     public void promiscuityTest() {
-        final String expectedIncoming = "INCOMING";
-        final String expectedOutgoing = "OUTGOING";
-
+//        final String expectedIncoming = "INCOMING";
+//        final String expectedOutgoing = "OUTGOING";
+        System.out.println(String.format("CREATE p=(s:Node {name:'degree3'})-[r:Edge]->(a:Node {name:'a%d'})",1));
         try(Session session = driver.session()) {
-            session.run(String.format("CREATE (:Person)-[:%s]->(:Movie {id:1})<-[:%s]-(:Person)", expectedIncoming, expectedIncoming));
-            session.run(String.format("MATCH (m:Movie {id:1}) CREATE (:Person)<-[:%s]-(m)-[:%s]->(:Person)", expectedOutgoing, expectedOutgoing));
+            session.run("CREATE (s:Node {name:'source'})");
+            session.run("CREATE (t:Node {name:'tail'})");
+            //System.out.println(session.run("MATCH (n) RETURN COUNT(DISTINCT(n))").single());
+            session.run("MATCH (n {name:'source'}) CREATE p=(n)-[r:Edge]->(degree3:Node {name:'degree3'})");
+            //System.out.println(session.run("MATCH (n) RETURN COUNT(DISTINCT(n))").single());
+            for(int i=0;i<2;i++){
+                session.run(String.format("MATCH (n:Node {name:'degree3'}) CREATE p=(n)-[r:Edge]->(a:Node {name:'a%d'})",i));
+                //System.out.println(session.run("MATCH (n) RETURN COUNT(DISTINCT(n))").single());
+            }
+            session.run("MATCH (n:Node {name:'source'}) CREATE p=(n)-[r:Edge]->(degree5:Node {name:'degree5'})");
+            for(int i=0;i<4;i++){
+                session.run(String.format("MATCH (n:Node {name:'degree5'}) CREATE p=(n)-[r:Edge]->(a:Node {name:'b%d'})",i));
+            }
+            session.run("MATCH (n:Node {name:'source'})  CREATE p=(n)-[r:Edge]->(degree10:Node {name:'degree10'})");
+            for(int i=0;i<9;i++){
+                session.run(String.format("MATCH (n:Node {name:'degree10'}) CREATE p=(n)-[r:Edge]->(a:Node {name:'c%d'})",i));
+            }
 
-            Record record = session.run("MATCH (u:Movie {id:1}) CALL example.promiscuityPath(u) YIELD outgoing, incoming RETURN outgoing, incoming").single();
+            List<Record> records = session.run("MATCH (n {name:'source'})-[r]->(m) RETURN m.name,SIZE((m)--())").list();
+            Record recordx = session.run("MATCH (s {name:'source'}), (t {name:'tail'}) RETURN s.name, t.name").single();
+            List<Record> records1 = session.run("MATCH (s {name:'source'}), (t {name:'tail'}) CALL example.promiscuityPath(s,t) YIELD promiscuity_score RETURN promiscuity_score").list();
 
-            assertThat(record.get("incoming").asList(x -> x.asString())).containsOnly();
-            assertThat(record.get("outgoing").asList(x -> x.asString())).containsOnly();
         }
     }
 
     /**
-     * We should only get distinct values from our procedure
+     * Tests comparator methods
      */
     @Test
     public void queueEntryTest() {
         final Promiscuity.Entry degree_5 = new Promiscuity.Entry(5,2,3,null);
         final Promiscuity.Entry degree_10 = new Promiscuity.Entry(10,2,3,null);
 
-        assertEquals(degree_5.compareTo(degree_10),5);
-//        assertThat(degree_5.compareTo(degree_10)).is(5);
-        //assertThat(record.get("outgoing").asList(x -> x.asString())).containsOnly();
+        final Promiscuity.Entry degree_5_equal_test = new Promiscuity.Entry(5,6,8,null);
+
+        Assert.assertTrue(degree_10.compareTo(degree_5) > 0);
+        Assert.assertTrue(degree_5.compareTo(degree_10) < 0);
+        Assert.assertEquals(degree_5_equal_test.compareTo(degree_5),0);
+
     }
+
+    /**
+     * Ensure that the comparator method for Entry is functioning for priority queue.
+     */
+    @Test
+    public void priorityQueueTest() {
+        final Promiscuity.Entry degree_5 = new Promiscuity.Entry(5,2,3,null);
+        final Promiscuity.Entry degree_10 = new Promiscuity.Entry(10,2,3,null);
+        final Promiscuity.Entry degree_1 = new Promiscuity.Entry(1,7,5,null);
+
+        PriorityQueue<Promiscuity.Entry> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(degree_5);
+        priorityQueue.add(degree_10);
+        priorityQueue.add(degree_1);
+
+        Promiscuity.Entry queue_first = priorityQueue.poll();
+        Promiscuity.Entry queue_second = priorityQueue.poll();
+        Promiscuity.Entry queue_third = priorityQueue.poll();
+        Promiscuity.Entry queue_fourth = priorityQueue.poll();
+
+        Assert.assertEquals(degree_1,queue_first); //Lowest degree should be first dequeued.
+        Assert.assertEquals(degree_5,queue_second);
+        Assert.assertEquals(degree_10,queue_third);
+        Assert.assertEquals(null,queue_fourth);
+
+    }
+
+
 }
