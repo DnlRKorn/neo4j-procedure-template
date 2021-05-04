e9603db61d61a66d112daf8da47cbee7df63d0d8
dnlrkorn <dnlrkorn@gmail.com>
2021-05-04 17:36:24 -0400

    Commit for evening.

--- src/main/java/example/Promiscuity.java
+++ src/main/java/example/Promiscuity.java
@@ -1,6 +1,5 @@
 package example;
 
-import org.neo4j.graphdb.Entity;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Path;
 import org.neo4j.graphdb.Relationship;
@@ -11,9 +10,6 @@ import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
 
 import org.neo4j.graphalgo.impl.util.PathImpl;
-
-import javax.management.relation.Relation;
-
 import static org.neo4j.graphalgo.impl.util.PathImpl.Builder;
 
 import java.util.*;
@@ -101,14 +97,20 @@ public class Promiscuity {
      * @param k_input    length of paths. If k=2 s->v1->v2->t would be a valid path. Must be of type Number to satisfy Neo4j.
      * @return An Output instance with the lowest scoring promiscuity score fo the pathway.
      */
-    @Procedure(value = "promiscuity.promiscuityScore")
+    @Procedure(value = "promiscuity.promiscuityPath")
     @Description("Get the lowest promiscuity score of paths of length k connecting a source and tail node.")
     public Stream<PathOutput> promiscuityPath(
             @Name("sourceNode") Node sourceNode,
             @Name("tailNode") Node tailNode,
-            @Name("k") Number k_input) {
-        ArrayList<PathOutput> result = new ArrayList<>();
+            @Name("k") Number k_input,
+            @Name("numPaths") Number numPaths_input) {
         int k = k_input.intValue();
+        int numPaths = numPaths_input.intValue();
+        ArrayList<PathOutput> results = new ArrayList<>(numPaths*2+5);
+        PathOutput emptyResult = new PathOutput(Integer.MAX_VALUE, null);
+
+        for(int i=0;i<numPaths;i++) results.add(emptyResult);
+
         PriorityQueue<PathEntry> priorityQueue = new PriorityQueue<>();
 
         //We cannot use the promiscuity_subroutine to enqueue the neighbors of the source node, as the degree of the
@@ -117,28 +119,34 @@ public class Promiscuity {
         sourceNode.getRelationships().iterator()
                 .forEachRemaining(rel -> AddToQueue(priorityQueue, rel.getOtherNode(sourceNode), 0, 1,sourceNodeEntry));
 
-        int best_score = Integer.MAX_VALUE;
+        int enqueuedPaths = 0;
         while (!priorityQueue.isEmpty()) {
+            int best_score = results.get(numPaths-1).promiscuity_score.intValue();
+
             PathEntry head = priorityQueue.poll();
-            if (head.degree >= best_score) {
-                break;
-            }
+            assert head != null;
+            if (head.degree >= best_score) break;
             int x = promiscuityPath_subroutine(head, tailNode, k, priorityQueue);
             if (x != -1) {
-                best_score = min(best_score, x);
                 Path p = buildPath(head, tailNode);
-                result.add(new PathOutput(best_score, p));
+                results.add(new PathOutput(x, p));
+                enqueuedPaths++;
+                Collections.sort(results);
             }
         }
 
-        return result.stream();
+        //We only want to return EITHER number of enqueued paths (i.e. if there are only three possible paths and
+        // the user requested five, we should return five.) OR the lowest promiscuity score paths up to numPaths.
+        int numResults = min(numPaths,enqueuedPaths);
+
+        return results.subList(0,numResults).stream();
     }
 
     private Path buildPath(PathEntry head, Node tail) {
-        ArrayList<Node> nodeArrayList = new ArrayList<Node>();
+        ArrayList<Node> nodeArrayList = new ArrayList<>();
         nodeArrayList.add(tail);
-        for(Node n = head.node; n!=null; head=head.parent){
-            nodeArrayList.add(n);
+        for(PathEntry e = head; e!=null; e=e.parent){
+            nodeArrayList.add(e.node);
         }
         Collections.reverse(nodeArrayList);
         Iterator<Node> nodeIterator = nodeArrayList.iterator();
@@ -147,7 +155,9 @@ public class Promiscuity {
         Node currentPathEnd = sourceNode;
         while(nodeIterator.hasNext()){
             Node nextAdditionToPath = nodeIterator.next();
-            builder.push(getRelationship(currentPathEnd,nextAdditionToPath));
+            Relationship rel = getRelationship(currentPathEnd,nextAdditionToPath);
+            assert rel != null;
+            builder = builder.push(rel);
             currentPathEnd = nextAdditionToPath;
         }
         return builder.build();
@@ -160,28 +170,6 @@ public class Promiscuity {
         return null;
     }
 
-    /*
-    private PathExpander<Double> buildPathExpander(String relationshipsAndDirections) {
-        PathExpanderBuilder builder = PathExpanderBuilder.empty();
-        for (Pair<RelationshipType, Direction> pair : RelationshipTypeAndDirections
-                .parse(relationshipsAndDirections)) {
-            if (pair.first() == null) {
-                if (pair.other() == null) {
-                    builder = PathExpanderBuilder.allTypesAndDirections();
-                } else {
-                    builder = PathExpanderBuilder.allTypes(pair.other());
-                }
-            } else {
-                if (pair.other() == null) {
-                    builder = builder.add(pair.first());
-                } else {
-                    builder = builder.add(pair.first(), pair.other());
-                }
-            }
-        }
-        return builder.build();
-    }
-    */
     /**
      * This procedure serves to
      **/
@@ -234,7 +222,7 @@ public class Promiscuity {
         }
     }
 
-    public static class PathOutput {
+    public static class PathOutput implements Comparator<PathOutput>, Comparable<PathOutput> {
         public final Number promiscuity_score;
         public final Path promiscuity_path;
 
@@ -245,6 +233,16 @@ public class Promiscuity {
 
         public PathOutput(int promiscuity_score, Path promiscuity_path) {
             this((Number) promiscuity_score, promiscuity_path);
+        }
+
+        @Override
+        public int compareTo(PathOutput o) {
+            return this.promiscuity_score.intValue() - o.promiscuity_score.intValue();
+        }
+
+        @Override
+        public int compare(PathOutput p1, PathOutput p2) {
+            return p1.promiscuity_score.intValue() - p2.promiscuity_score.intValue();
         }
     }
 
--- src/test/java/example/PromiscuityTests.java
+++ src/test/java/example/PromiscuityTests.java
@@ -1,14 +1,22 @@
 package example;
 
+import org.apache.commons.collections.IteratorUtils;
 import org.junit.jupiter.api.*;
 import org.neo4j.driver.Record;
 import org.neo4j.driver.Config;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
 import org.neo4j.driver.Session;
+import org.neo4j.driver.internal.InternalPath;
+import org.neo4j.driver.internal.value.PathValue;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Path;
 import org.neo4j.harness.Neo4j;
 import org.neo4j.harness.Neo4jBuilders;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.PriorityQueue;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -87,6 +95,58 @@ public class PromiscuityTests {
         }
     }
 
+    @Test
+    public void promiscuityPathTest() {
+
+        try(Session session = driver.session()) {
+            session.run("CREATE (s:Node {name:'source'})");
+            session.run("CREATE (t:Node {name:'tail'})");
+
+            session.run("MATCH (s {name:'source'}) CREATE p=(s)-[r:Edge]->(degree3:Node {name:'degree3'})");
+            session.run("MATCH (n:Node {name:'degree3'}), (t:Node {name:'tail'}) CREATE (n)-[r:Edge]->(t)");
+            //We want "degree3" to have node.degree() == 3. Has a 2 edges to source and tail, create 3 - 2 additional
+            // connections.
+            for(int i=0;i<3-2;i++){
+                session.run(String.format("MATCH (n:Node {name:'degree3'}) CREATE p=(n)-[r:Edge]->(a:Node {name:'a%d'})",i));
+                //System.out.println(session.run("MATCH (n) RETURN COUNT(DISTINCT(n))").single());
+            }
+            session.run("MATCH (s:Node {name:'source'}) CREATE p=(s)-[r:Edge]->(degree5:Node {name:'degree5'})");
+            session.run("MATCH (n:Node {name:'degree5'}), (t:Node {name:'tail'}) CREATE (n)-[r:Edge]->(t)");
+            for(int i=0;i<5-2;i++){
+                session.run(String.format("MATCH (n:Node {name:'degree5'}) CREATE p=(n)-[r:Edge]->(a:Node {name:'b%d'})",i));
+            }
+            session.run("MATCH (s:Node {name:'source'})  CREATE p=(s)-[r:Edge]->(degree10:Node {name:'degree10'})");
+            session.run("MATCH (n:Node {name:'degree10'}), (t:Node {name:'tail'}) CREATE (n)-[r:Edge]->(t)");
+            for(int i=0;i<10-2;i++){
+                session.run(String.format("MATCH (n:Node {name:'degree10'}) CREATE p=(n)-[r:Edge]->(a:Node {name:'c%d'})",i));
+            }
+
+//            Record record = session.run("MATCH (s {name:'source'}), (t {name:'tail'}) CALL " +
+//                    "promiscuity.promiscuityScore(s,t,1) YIELD promiscuity_score RETURN promiscuity_score").single();
+//            assertEquals(record.get("promiscuity_score").asInt(),3);
+
+            //Remove the connection from degree3 and tail. Expect new promiscuity_score of graph to be 5.
+            //session.run("MATCH (n:Node {name:'degree3'})-[r:Edge]->(t:Node {name:'tail'}) DELETE r");
+            List<Record> record_list = session.run("MATCH (s {name:'source'}), (t {name:'tail'}) CALL promiscuity.promiscuityPath(s,t,1,3) YIELD promiscuity_score, promiscuity_path RETURN promiscuity_score, promiscuity_path").list();
+            Record record = record_list.get(0);
+
+            assertEquals(record.get("promiscuity_score").asInt(),3);
+            InternalPath p = (InternalPath) record.get("promiscuity_path").asPath();
+            Object[] l = IteratorUtils.toArray(p.nodes().iterator());
+            Node aa = (Node) l[1];
+            assertEquals(aa.getProperty("name"),"degree3");
+
+        //    assertEquals(record.get("promiscuity_score").asInt(),5);
+       //     PathValue p = (PathValue) record.get("promiscuity_path");
+       //     System.out.println(p.asPath().length());
+        }
+    }
+
+    private Node getSecondNode(Path p){
+        Iterator<Node> nodeIterator = p.nodes().iterator();
+        nodeIterator.next();
+        return nodeIterator.next();
+    }
     /**
      * Tests comparator methods of Entry nodes which we leverage in the priority queue. Should leverage degree as
      * key for comparison.
