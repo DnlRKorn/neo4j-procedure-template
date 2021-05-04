b27989132de8031be18c2559587d087745ff7afd
dnlrkorn <dnlrkorn@gmail.com>
2021-05-04 13:21:50 -0400

    Untested version of promiscuity path method.

--- src/main/java/example/Promiscuity.java
+++ src/main/java/example/Promiscuity.java
@@ -1,16 +1,22 @@
 package example;
 
+import org.neo4j.graphdb.Entity;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Path;
+import org.neo4j.graphdb.Relationship;
 import org.neo4j.logging.Log;
 import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
 
+import org.neo4j.graphalgo.impl.util.PathImpl;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import javax.management.relation.Relation;
+
+import static org.neo4j.graphalgo.impl.util.PathImpl.Builder;
+
+import java.util.*;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -19,9 +25,8 @@ import static java.lang.Math.min;
 
 
 /**
- * This is an example showing how you could expose Neo4j's full text indexes as
- * two procedures - one for updating indexes, and one for querying by label and
- * the lucene query language.
+ * The procedures here aim to provide fast and efficent methods for finding low promiscuity scores and associated
+ * pathways within Neo4J graphs.
  */
 public class Promiscuity {
     // This gives us a log instance that outputs messages to the
@@ -30,12 +35,12 @@ public class Promiscuity {
     public Log log;
 
     /**
-     * This procedure takes a Node and gets the relationships going in and out of it
+     * This procedure takes a source Node, a tail Node, and a length parameter k.
      *
      * @param sourceNode node to start promiscuity search from
      * @param tailNode   node to end promiscuity search at
      * @param k_input    length of paths. If k=2 s->v1->v2->t would be a valid path. Must be of type Number to satisfy Neo4j.
-     * @return A RelationshipTypes instance with the relations (incoming and outgoing) for a given node.
+     * @return An Output instance with the lowest scoring promiscuity score fo the pathway.
      */
     @Procedure(value = "promiscuity.promiscuityScore")
     @Description("Get the lowest promiscuity score of paths of length k connecting a source and tail node.")
@@ -60,7 +65,7 @@ public class Promiscuity {
                 result.add(new Output(best_score));
                 break;
             }
-            int x = promiscuity_subroutine(node, tailNode, head.depth, k, head.path_score, priorityQueue);
+            int x = promiscuityScore_subroutine(node, tailNode, head.depth, k, head.path_score, priorityQueue);
             if (x != -1) {
                 best_score = min(best_score, x);
             }
@@ -69,7 +74,11 @@ public class Promiscuity {
         return result.stream();
     }
 
-    private int promiscuity_subroutine(Node node, Node tail, int depth, int k, int path_score, PriorityQueue<Entry> priorityQueue) {
+
+    /**
+     * This procedure serves to
+     **/
+    private int promiscuityScore_subroutine(Node node, Node tail, int depth, int k, int path_score, PriorityQueue<Entry> priorityQueue) {
         int updated_path_score = max(node.getDegree(), path_score);
         if (depth == k) {
             boolean tail_neighbor = StreamSupport.stream(node.getRelationships().spliterator(), false)
@@ -85,6 +94,113 @@ public class Promiscuity {
     }
 
     /**
+     * This procedure takes a source Node, a tail Node, and a length parameter k.
+     *
+     * @param sourceNode node to start promiscuity search from
+     * @param tailNode   node to end promiscuity search at
+     * @param k_input    length of paths. If k=2 s->v1->v2->t would be a valid path. Must be of type Number to satisfy Neo4j.
+     * @return An Output instance with the lowest scoring promiscuity score fo the pathway.
+     */
+    @Procedure(value = "promiscuity.promiscuityScore")
+    @Description("Get the lowest promiscuity score of paths of length k connecting a source and tail node.")
+    public Stream<PathOutput> promiscuityPath(
+            @Name("sourceNode") Node sourceNode,
+            @Name("tailNode") Node tailNode,
+            @Name("k") Number k_input) {
+        ArrayList<PathOutput> result = new ArrayList<>();
+        int k = k_input.intValue();
+        PriorityQueue<PathEntry> priorityQueue = new PriorityQueue<>();
+
+        //We cannot use the promiscuity_subroutine to enqueue the neighbors of the source node, as the degree of the
+        // source node has no effect on the promiscuity score of paths.
+        PathEntry sourceNodeEntry = new PathEntry(0,0,0, sourceNode,null);
+        sourceNode.getRelationships().iterator()
+                .forEachRemaining(rel -> AddToQueue(priorityQueue, rel.getOtherNode(sourceNode), 0, 1,sourceNodeEntry));
+
+        int best_score = Integer.MAX_VALUE;
+        while (!priorityQueue.isEmpty()) {
+            PathEntry head = priorityQueue.poll();
+            if (head.degree >= best_score) {
+                break;
+            }
+            int x = promiscuityPath_subroutine(head, tailNode, k, priorityQueue);
+            if (x != -1) {
+                best_score = min(best_score, x);
+                Path p = buildPath(head, tailNode);
+                result.add(new PathOutput(best_score, p));
+            }
+        }
+
+        return result.stream();
+    }
+
+    private Path buildPath(PathEntry head, Node tail) {
+        ArrayList<Node> nodeArrayList = new ArrayList<Node>();
+        nodeArrayList.add(tail);
+        for(Node n = head.node; n!=null; head=head.parent){
+            nodeArrayList.add(n);
+        }
+        Collections.reverse(nodeArrayList);
+        Iterator<Node> nodeIterator = nodeArrayList.iterator();
+        Node sourceNode = nodeIterator.next();
+        PathImpl.Builder builder = new Builder(sourceNode);
+        Node currentPathEnd = sourceNode;
+        while(nodeIterator.hasNext()){
+            Node nextAdditionToPath = nodeIterator.next();
+            builder.push(getRelationship(currentPathEnd,nextAdditionToPath));
+            currentPathEnd = nextAdditionToPath;
+        }
+        return builder.build();
+    }
+
+    private Relationship getRelationship(Node a, Node b){
+        for( Relationship rel : a.getRelationships()){
+            if(rel.getOtherNode(a).equals(b)) return rel;
+        }
+        return null;
+    }
+
+    /*
+    private PathExpander<Double> buildPathExpander(String relationshipsAndDirections) {
+        PathExpanderBuilder builder = PathExpanderBuilder.empty();
+        for (Pair<RelationshipType, Direction> pair : RelationshipTypeAndDirections
+                .parse(relationshipsAndDirections)) {
+            if (pair.first() == null) {
+                if (pair.other() == null) {
+                    builder = PathExpanderBuilder.allTypesAndDirections();
+                } else {
+                    builder = PathExpanderBuilder.allTypes(pair.other());
+                }
+            } else {
+                if (pair.other() == null) {
+                    builder = builder.add(pair.first());
+                } else {
+                    builder = builder.add(pair.first(), pair.other());
+                }
+            }
+        }
+        return builder.build();
+    }
+    */
+    /**
+     * This procedure serves to
+     **/
+    private int promiscuityPath_subroutine(PathEntry entry, Node tail, int k, PriorityQueue<PathEntry> priorityQueue) {
+        Node node = entry.node;
+        int updated_path_score = max(node.getDegree(), entry.path_score);
+        if (entry.depth == k) {
+            boolean tail_neighbor = StreamSupport.stream(node.getRelationships().spliterator(), false)
+                    .anyMatch(rel -> rel.getOtherNode(node).equals(tail));
+            if (tail_neighbor) return updated_path_score;
+            else return -1;
+        } else {
+            node.getRelationships().iterator()
+                    .forEachRemaining(rel -> AddToQueue(priorityQueue, rel.getOtherNode(node), updated_path_score,entry.depth + 1,entry));
+        }
+        return -1;
+    }
+
+    /**
      * Creates entry for given node and appends to priority queue.
      *
      * @param priorityQueue the queue which we are adding the Entry to.
@@ -92,6 +208,17 @@ public class Promiscuity {
      */
     private void AddToQueue(PriorityQueue<Entry> priorityQueue, Node node, int path_score, int depth) {
         Entry e = new Entry(node.getDegree(), path_score, depth, node);
+        priorityQueue.add(e);
+    }
+
+    /**
+     * Creates entry for given node and appends to priority queue.
+     *
+     * @param priorityQueue the queue which we are adding the Entry to.
+     * @param node          the node which we should create Entry for and append to queue.
+     */
+    private void AddToQueue(PriorityQueue<PathEntry> priorityQueue, Node node, int path_score, int depth, PathEntry parent) {
+        PathEntry e = new PathEntry(node.getDegree(), path_score, depth, node, parent);
         priorityQueue.add(e);
     }
 
@@ -103,7 +230,21 @@ public class Promiscuity {
         }
 
         public Output(int promiscuity_score) {
-            this( (Number)promiscuity_score );
+            this((Number) promiscuity_score);
+        }
+    }
+
+    public static class PathOutput {
+        public final Number promiscuity_score;
+        public final Path promiscuity_path;
+
+        public PathOutput(Number promiscuity_score, Path promiscuity_path) {
+            this.promiscuity_score = promiscuity_score;
+            this.promiscuity_path = promiscuity_path;
+        }
+
+        public PathOutput(int promiscuity_score, Path promiscuity_path) {
+            this((Number) promiscuity_score, promiscuity_path);
         }
     }
 
@@ -131,6 +272,16 @@ public class Promiscuity {
             return e1.degree - e2.degree;
         }
 
+    }
+
+    public static class PathEntry extends Entry {
+        // These entries contain information we need about nodes stashed on our priority queue.
+        public final PathEntry parent;
+
+        public PathEntry(int degree, int path_score, int depth, Node node, PathEntry parent) {
+            super(degree, path_score, depth, node);
+            this.parent = parent;
+        }
     }
 
 }
